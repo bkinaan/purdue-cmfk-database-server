81ac6434536827ac769d5ee0143ffb0bd5df4499
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2024-05-19 22:21:37 -0400

    updated user models

--- models/Mentor.js
+++ models/Mentor.js
@@ -5,12 +5,12 @@ class Mentor extends Model {
   static fields = {
     username: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       unique: true,
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       unique: false,
     },
     FirstName: {
@@ -28,63 +28,17 @@ class Mentor extends Model {
       allowNull: false,
       unique: true,
     },
-    NonSchoolEmailAddress: {
+    StaffRole: {
       type: DataTypes.STRING,
       allowNull: true,
       unique: false,
     },
-    Phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    ActivityDays: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: false,
-    },
-    PrimaryStaffRole: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: false,
-    },
-    SecondaryStaffRole: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: false,
-    },
-    Paired: {
+    PairedWith: {
       type: DataTypes.STRING,
       allowNull: true,
       unique: false,
     },
   };
-
-  // static hooks = {
-  //   beforeCreate: async (mentor, options) => {
-  //     console.log("BEFORE CREATE");
-  //     try {
-  //       mentor.password = await bcrypt.hash(
-  //         mentor.password,
-  //         parseInt(process.env.SALT_ROUNDS)
-  //       );
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   },
-  //   beforeUpdate: async (mentor, options) => {
-  //     console.log("BEFORE UPDATE");
-  //     try {
-  //       mentor.password = await bcrypt.hash(
-  //         mentor.password,
-  //         parseInt(process.env.SALT_ROUNDS)
-  //       );
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   },
-  // };
-
   async validPassword(password) {
     try {
       // compares passed in password with hashed password stored in mentor
@@ -100,7 +54,6 @@ class Mentor extends Model {
     }
   }
 
-  // remove password from mentor before sending to client
   toJSON() {
     const values = Object.assign({}, this.get());
     delete values.password;
